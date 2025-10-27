47b02ec512e88a09ac4dab3bbdebc8b0bf54d041
zhonglinlin <2194785560@qq.com>
2025-10-28 07:42:16 +0800

    初始化新分支

--- src/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { createRoot } from "react-dom/client";
-import Render2, { Render2Index } from './up/render/Render2';
-import Render3 from './up/render/Render3';
-const root = createRoot(document.getElementById("root"));
-root.render(<Render3 />);
